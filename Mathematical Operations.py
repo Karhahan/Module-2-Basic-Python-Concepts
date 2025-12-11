78180d6e0fd9fe9bca7e0cad8f8bccb5c64bfecb
Karan  S <karan.singh@bca.christuniversity.in>
2025-12-11 18:46:20 +0530

    Add files via upload

---
+++ Mathematical Operations.py
@@ -0,0 +1,9 @@
+num1 = int(input(f" Please enter the first number:"))
+num2 = int(input(f"Please enter the second number:"))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print(f" addition:", add, '\n', "sub:", sub, '\n', "mul:", mul, '\n', "div:", div )
